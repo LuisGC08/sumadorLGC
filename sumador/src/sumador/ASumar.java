8c288876ba47e79e5aebda8e77c9a855a6ed00fa
Salvador Galindo Martínez <salva150809@gmail.com>
2019-03-03 20:39:57 +0100

    Añadida primera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,31 @@
 package sumador;
 
 public class ASumar {
-
+	
+	String valorInicial;
+	/**
+	 * Método constructor para almacenar la variable
+	 * @author Salvador Galindo Martínez
+	 * @param string
+	 * @version 1.0
+	 */
+	public ASumar(String string) {
+		this.valorInicial = string;
+	}
+	
+	/**
+	 * Método que mide la longitud de la variable introducida 
+	 * y muestra el resultado
+	 * @author Salvador Galindo Martínez
+	 * @version 1.0
+	 * @return devuelve el resultado correcto si cumple la condición y si no, 
+	 * devuelve un mensaje de error
+	 */
+	public String mostrar() {
+		String numero = valorInicial;
+		if (numero.length() == 1) {
+			return (numero + " = " + numero);
+		}
+		return ("Número incorrecto");
+	}
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class TestASumar {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUnaCifra() {
+		ASumar sumi= new ASumar("6");
+		String resultado = sumi.mostrar();
+		assertEquals("6 = 6", resultado);
 	}
 
 }

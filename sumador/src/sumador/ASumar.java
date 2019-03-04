dd14e7ea211e5f7834a4124fbb694ade4578be1a
Salvador Galindo Martínez <salva150809@gmail.com>
2019-03-04 18:40:46 +0100

    Mezclado bien primera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -14,6 +14,22 @@ public class ASumar {
 	}
 	
 	/**
+	 * Método que mide la longitud de la variable introducida 
+	 * y muestra el resultado
+	 * @author Salvador Galindo Martínez
+	 * @version 1.0
+	 * @return devuelve el resultado correcto si cumple la condición y si no, 
+	 * devuelve un mensaje de error
+	 */
+	public String mostrar() {
+		String numero = valorInicial;
+		if (numero.length() == 1) {
+			return (numero + " = " + numero);
+		}
+		return ("Número incorrecto");
+	}	
+	
+	/**
 	 * Método que muestra una cadena de texto vacía si el número introducido
 	 * es negativo.
 	 * @author Salvador Galindo Martínez
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -5,7 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestASumar {
-
+	
+	@Test
+	public void testUnaCifra() {
+		ASumar sumi= new ASumar("6");
+		String resultado = sumi.mostrar();
+		assertEquals("6 = 6", resultado);
+	}
+	
 	@Test
 	public void testNegativo() {
 		ASumar sumi = new ASumar("-2");

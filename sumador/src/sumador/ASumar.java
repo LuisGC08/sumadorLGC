ea0696afb52e5c1b74ec3b06235e039af6486681
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2020-03-09 09:16:30 +0100

    primera prueba con javadoc

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -3,11 +3,22 @@ package sumador;
 public class ASumar {
 	
 	String valorInicial;
-	
+	/**
+	 * Método constructor para almacenar la variable
+	 * @author Luis Garcia Clavel
+	 * @param string
+	 * @version 1.0
+	 */
 	public ASumar(String string) {
 		this.valorInicial = string;
 	}
 	
+	/**
+	 * Método que mide la longitud de la variable introducida 
+	 * y muestra el resultado
+	 * @return devuelve el resultado correcto si cumple la condición y si no, 
+	 * devuelve un mensaje de error
+	 */
 	public String mostrar() {
 		String numero = valorInicial;
 		if (numero.length() == 1) {

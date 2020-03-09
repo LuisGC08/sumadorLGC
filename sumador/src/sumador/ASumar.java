5e3baaccb56fb81b02dc2bf9a5419e84f949b21f
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2020-03-09 09:17:32 +0100

    segunda prueba con javadoc

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -3,12 +3,23 @@ package sumador;
 public class ASumar {
 	
 	String valorInicial;
-	
+	/**
+	 * Método constructor para almacenar la variable
+	 * @author Salvador Galindo Martínez
+	 * @param string
+	 * @version 1.0
+	 */
 	public ASumar(String string) {
 		this.valorInicial = string;
 	}
 	
-	
+	/**
+	 * Método que muestra una cadena de texto vacía si el número introducido
+	 * es negativo.
+	 * @author Luis Garcia Clavel
+	 * @version 1.0
+	 * @return 
+	 */
 	public String mostrarNegativo() {
 		String numero = valorInicial;
 		if (Integer.parseInt(numero) > 0) {

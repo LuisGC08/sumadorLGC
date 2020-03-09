0e667297d707783290463f0934aecf9f024f7dab
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2020-03-09 08:33:07 +0100

    realizamos la primera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -8,39 +8,11 @@ public class ASumar {
 		this.valorInicial = string;
 	}
 	
-	
 	public String mostrar() {
 		String numero = valorInicial;
 		if (numero.length() == 1) {
 			return (numero + " = " + numero);
 		}
 		return ("Número incorrecto");
-	}	
-	
-	
-	public String mostrarNegativo() {
-		String numero = valorInicial;
-		if (Integer.parseInt(numero) > 0) {
-			return (numero + " = " + numero);
-		}else {
-			return ("");
-		}
 	}
-	
-	
-	public String total() {
-		int suma = 0;
-		String fin = "";
-		for(int i = 0; i < valorInicial.length(); i++) {
-			String digito = valorInicial.substring(i, i+1);
-			suma += Integer.parseInt(digito);
-			if (i < 1) {
-				fin += digito;
-			}else {
-				fin += " + " + digito;
-			}
-		}
-		return (fin + " = " + suma);
-	}
-	
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -5,26 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestASumar {
-	
+
 	@Test
 	public void testUnaCifra() {
 		ASumar sumi= new ASumar("6");
 		String resultado = sumi.mostrar();
 		assertEquals("6 = 6", resultado);
 	}
-	
-	@Test
-	public void testNegativo() {
-		ASumar sumi = new ASumar("-2");
-		String resultado = sumi.mostrarNegativo();
-		assertEquals("", resultado);
-	}
-	
-	@Test
-	public void testSuma() {
-		ASumar sumi = new ASumar("123");
-		String resultado = sumi.total();
-		assertEquals("1 + 2 + 3 = 6",resultado);
-	}
 
-}
+
